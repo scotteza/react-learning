1db582f83f1a67c3b3e8b5183b7c5de6318d6732
Scott Edwards <scotteza@gmail.com>
2018-01-25 14:57:41 +0200

    Added Button component.

--- src/App.js
+++ src/App.js
@@ -114,9 +114,10 @@ class App extends Component {
 
 class Search extends Component {
   render() {
-    const { value, onChange } = this.props;
+    const { value, onChange, children } = this.props;
     return (
       <form>
+        {children}
         <input type="text" value={value} onChange={onChange} />
       </form>
     );
@@ -137,13 +138,30 @@ class Table extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button onClick={() => onDismiss(item.objectID)} type="button">
+              <Button onClick={() => onDismiss(item.objectID)} type="button">
                 Dismiss
-              </button>
+              </Button>
             </span>
           </div>
         ))}
       </div>
+    );
+  }
+}
+
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      // Defaults when destructuring
+      className = "",
+      children
+    } = this.props;
+
+    return (
+      <button onClick={onClick} className={className} type="button">
+        {children}
+      </button>
     );
   }
 }

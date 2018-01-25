d54389bbae7fae0137447b363f60e0cd3d79a4c7
Scott Edwards <scotteza@gmail.com>
2018-01-25 17:16:58 +0200

    Finished chapter 2

--- src/App.js
+++ src/App.js
@@ -1,47 +1,25 @@
-import React, { Component } from "react";
-import "./App.css";
-import Developer from "./Developer";
-import IAmAStatelessFunctionalComponent from "./IAmAStatelessFunctionalComponent";
-import ExplainBindingsComponent from "./ExplainBindingsComponent";
-// import Table from "./components/Table";
-// import Search from "./components/Search";
-
-// Basic React forms: https://reactjs.org/docs/forms.html
-
-const DEFAULT_QUERY = "redux";
-const PATH_BASE = "https://hn.algolia.com/api/v1";
-const PATH_SEARCH = "/search";
-const PARAM_SEARCH = "query=";
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
-console.log(url);
+import React, { Component } from 'react';
+import './App.css';
 
 const list = [
   {
-    title: "React",
-    url: "https://facebook.github.io/react/",
-    author: "Jordan Walke",
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
     num_comments: 3,
     points: 4,
-    objectID: 0
+    objectID: 0,
   },
   {
-    title: "Redux",
-    url: "https://github.com/reactjs/redux",
-    author: "Dan Abramov, Andrew Clark",
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
     points: 5,
-    objectID: 1
-  }
+    objectID: 1,
+  },
 ];
 
-// ES5 way
-// function isSearched(searchTerm) {
-//   return function(item) {
-//     return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-//   };
-// }
-
-// ES6 way
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
@@ -51,123 +29,93 @@ class App extends Component {
 
     this.state = {
       list,
-      searchTerm: ""
+      searchTerm: '',
     };
 
-    this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
-
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-    // Destructuring 1
-    const user = {
-      firstname: "Robin",
-      lastname: "Wieruch"
-    };
-    const { firstname, lastname } = user;
-    console.log(firstname, lastname);
-    // Destructuring 2
-    const users = ["Robin", "Andrew", "Dan"];
-    const [u1, u2, u3] = users;
-    console.log(u1, u2, u3);
+    this.onDismiss = this.onDismiss.bind(this);
   }
 
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
-    // console.log(this.state.searchTerm);
-    // console.log(event.target.value);
   }
 
   onDismiss(id) {
-    // Define an isNotId function.
-    // Not that this is an arrow function with a single param, so we don't need to say (item) and it has an implicit "return" statement
     const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list.filter(isNotId);
-    // We could also say:
-    // const updatedList = this.state.list.filter(item => item.objectID !== id);
-
     this.setState({ list: updatedList });
   }
 
   render() {
-    // Using destructuring here
     const { searchTerm, list } = this.state;
-
-    // Same as saying:
-    // var searchTerm = this.state.searchTerm;
-    // var list = this.state.list;
-
     return (
       <div className="page">
         <div className="interactions">
-          <Search value={searchTerm} onChange={this.onSearchChange}>
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
             Search
           </Search>
         </div>
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
-        <div>
-          <Developer firstname="Bob" lastname="Smith" />
-        </div>
-        <div>
-          <Developer firstname="Jane" lastname="Jones" />
-        </div>
-        <div>
-          <IAmAStatelessFunctionalComponent firstname="Banana" lastname="Jam" />
-        </div>
-        <div>
-          <ExplainBindingsComponent />
-        </div>
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
     );
   }
 }
 
-const Search = ({ value, onChange, children }) => (
+const Search = ({ value, onChange, children }) =>
   <form>
-    {children}
-    <input type="text" value={value} onChange={onChange} />
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
   </form>
-);
 
-const Table = ({ list, pattern, onDismiss }) => {
-  const largeColumn = {
-    width: "40%"
-  };
-  const midColumn = {
-    width: "30%"
-  };
-  const smallColumn = {
-    width: "10%"
-  };
+const Table = ({ list, pattern, onDismiss }) =>
+  <div className="table">
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID} className="table-row">
+        <span style={{ width: '40%' }}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={{ width: '30%' }}>
+          {item.author}
+        </span>
+        <span style={{ width: '10%' }}>
+          {item.num_comments}
+        </span>
+        <span style={{ width: '10%' }}>
+          {item.points}
+        </span>
+        <span style={{ width: '10%' }}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+        </span>
+      </div>
+    )}
+  </div>
 
-  return (
-    <div className="table">
-      {list.filter(isSearched(pattern)).map(item => (
-        <div key={item.objectID} className="table-row">
-          <span style={largeColumn}>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span style={midColumn}>{item.author}</span>
-          <span style={smallColumn}>{item.num_comments}</span>
-          <span style={smallColumn}>{item.points}</span>
-          <span style={smallColumn}>
-            <Button
-              onClick={() => onDismiss(item.objectID)}
-              type="button"
-              className="button-inline"
-            >
-              Dismiss
-            </Button>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-const Button = ({ onClick, className = "", children }) => (
-  <button onClick={onClick} className={className} type="button">
+const Button = ({
+  onClick,
+  className = '',
+  children,
+}) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
     {children}
   </button>
-);
 
 export default App;

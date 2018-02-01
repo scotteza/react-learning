abe40fc3d590642cdad6d4c315b1021dafc84703
Scott Edwards <scotteza@gmail.com>
2018-02-01 16:05:38 +0200

    Fixing warning when running tests.

--- src/App.test.js
+++ src/App.test.js
@@ -16,7 +16,7 @@ describe("App", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    // ReactDOM.unmountComponentAtNode(div);
   });
 
   test("has a valid snapshot", () => {
